47a4b7d05689015de1e64366c66978bf859b1b46
Aaron Pritzlaff <aaron.pritzlaff@gmail.com>
2020-04-18 19:59:24 +0100

    run

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/env bash
 
-FILE="target/scala-2.13/balloons-assembly-0.0.1.jar"
+JARFILE="target/scala-2.13/balloons-assembly-0.0.1.jar"
 
 function dockerBuild {
   echo "building with docker"
-  docker run --mount type=bind,source="$(pwd)",target=/opt --rm hseeberger/scala-sbt:8u222_1.3.5_2.13.1  /bin/sh -c 'cd /opt; sbt clean assembly'
+  docker run --mount type=bind,source="$(pwd)",target=/opt --rm hseeberger/scala-sbt:8u222_1.3.5_2.13.1 /bin/sh -c 'cd /opt; sbt clean assembly'
 }
 
-if [[ -f "$FILE" ]]; then
-  echo "enter balloons (e.g. 4 2 1 or r123):"
+if [[ -f "$JARFILE" ]]; then
+  echo "enter balloon game scenario (e.g. 4 2 1 or r123):"
 else 
   echo "building..."
   
@@ -16,8 +16,7 @@ else
 
   sbt assembly
 
-  echo "enter balloons (e.g. 4 2 1 or r123):"
+  echo "enter balloon game scenario (e.g. 4 2 1 or r123):"
 fi;
 
-echo "args are $*"
-java -jar $FILE
+java -jar $JARFILE
